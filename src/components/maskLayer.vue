5d0416ec60926579a35d3ef0d56b6d08052e6947
Guo <962343237@qq.com>
2019-05-07 10:00:56 +0800

    test

---
+++ src/components/maskLayer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="maskDiv"></div>
+</template>
+
+<script>
+    export default {
+      name: "MaskLayer"
+    }
+</script>
+
+<style scoped>
+#maskDiv{
+  position:absolute;
+  top:0;left:0;
+  background-color: #000000;
+  opacity:0.7;
+  width: 100%;
+  height: 100%;
+  z-index: 9;
+}
+</style>
